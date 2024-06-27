e045ec4c26333ebfaa3cd6aba6cc9de1111ef226
planeflake <135433211+planeflake@users.noreply.github.com>
2024-06-28 01:11:12 +0200

    dm dashboard updated

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_socketio import SocketIO, emit
 from neo4j import GraphDatabase
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 # Neo4j connection
 uri = 'neo4j+s://b29956d6.databases.neo4j.io'
@@ -10,10 +13,10 @@ password = '0kc1APDksb8vIkSWOraGix4fulXDzr6d_81Uw5JLDbs'
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
 # Dummy player data
-players = [
+players_list = [
     {'username': 'player1'},
     {'username': 'player2'},
-    {'username': 'dm'}  # Adding the dm user here
+    {'username': 'dm'}
 ]
 
 # Jinja2 filter for calculating ability modifiers
@@ -23,19 +26,59 @@ def modifier_filter(score):
 
 app.jinja_env.filters['modifier'] = modifier_filter
 
-
 def get_characters_for_player(username):
     with driver.session() as session:
         result = session.run(
             "MATCH (p:Player {username: $username})-[:HAS_CHARACTER]->(c:Character) "
             "RETURN c.name AS name, id(c) AS id", username=username)
         characters = [{"name": record["name"], "id": record["id"]} for record in result]
-        print('Characters for player: ' + str(characters) + ' ' + str(username) + ' ' + str(result))
     return characters
+
+def get_conditions():
+    with driver.session() as session:
+        result = session.run("MATCH (c:Condition) RETURN c.name AS name, c.icon AS icon")
+        conditions = [{"name": record["name"], "icon": record["icon"]} for record in result]
+    return conditions
+
+def get_characters_for_testing():
+    with driver.session() as session:
+        result = session.run(
+            """
+            MATCH (c:Character)-[:BELONGS_TO_CLASS]->(cl:Class),
+                  (c)-[:BELONGS_TO_RACE]->(r:Races)
+            OPTIONAL MATCH (c)-[:HAS_AC]->(ac:ArmorClass)
+            OPTIONAL MATCH (c)-[:HAS_CR]->(cr:ChallengeRating)
+            OPTIONAL MATCH (c)-[:HAS_TYPE]->(type:Type)
+            RETURN c.name AS characterName, id(c) AS characterId, cl.name AS className, r.name AS raceName,
+                   c.level AS level, c.strength AS strength, c.dexterity AS dexterity, 
+                   c.constitution AS constitution, c.intelligence AS intelligence,
+                   c.wisdom AS wisdom, c.charisma AS charisma, c.token_url AS tokenUrl, 
+                   ac.ac AS ac, cr.cr AS cr, type.type AS type
+            """
+        )
+        characters = []
+        for record in result:
+            characters.append({
+                "name": record["characterName"],
+                "id": record["characterId"],
+                "class": record["className"],
+                "race": record["raceName"],
+                "level": record["level"],
+                "strength": record["strength"],
+                "dexterity": record["dexterity"],
+                "constitution": record["constitution"],
+                "intelligence": record["intelligence"],
+                "wisdom": record["wisdom"],
+                "charisma": record["charisma"],
+                "tokenUrl": record["tokenUrl"],
+                "ac": record["ac"],
+                "cr": record["cr"],
+                "type": record["type"]
+            })
+        return characters
 
 def get_character_details(character_id):
     with driver.session() as session:
-        print('Fetching character details for: ' + str(character_id) + ' ' + str(session))
         result = session.run(
             """
             MATCH (c:Character)-[:BELONGS_TO_CLASS]->(cl:Class),
@@ -64,12 +107,34 @@ def get_races():
     return races
 
 def get_alignments():
-    # Assuming alignments are stored as a list in the code
     return ['Lawful Good', 'Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'True Neutral', 'Chaotic Neutral', 'Lawful Evil', 'Neutral Evil', 'Chaotic Evil']
 
 def get_backgrounds():
-    # Assuming backgrounds are stored as a list in the code
     return ['Acolyte', 'Charlatan', 'Criminal', 'Entertainer', 'Folk Hero', 'Guild Artisan', 'Hermit', 'Noble', 'Outlander', 'Sage', 'Sailor', 'Soldier', 'Urchin']
+
+def get_five_monsters():
+    with driver.session() as session:
+        result = session.run(
+            """
+            MATCH (m:Monster)
+            OPTIONAL MATCH (m)-[:HAS_AC]->(ac:ArmorClass)
+            OPTIONAL MATCH (m)-[:HAS_CR]->(cr:ChallengeRating)
+            OPTIONAL MATCH (m)-[:HAS_TYPE]->(type:Type)
+            RETURN m.name AS name, ac.ac AS ac, cr.cr AS cr, type.type AS type, m.token_url as tokenUrl
+            LIMIT 5
+            """
+        )
+        monsters = []
+        for record in result:
+            monsters.append({
+                "name": record["name"],
+                "ac": record["ac"],
+                "cr": record["cr"],
+                "type": record["type"],
+                "tokenUrl": record["tokenUrl"]
+            })
+        return monsters
+
 
 @app.route('/')
 def home():
@@ -87,15 +152,15 @@ def load_character(character_id):
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
-    if username == 'dm':
-        return redirect(url_for('dm_dashboard', username=username))
-    elif any(player['username'] == username for player in players):
+    if any(player['username'] == username for player in players_list):
+        if username == 'dm':
+            return redirect(url_for('dm_dashboard', username=username))
         return redirect(url_for('player_dashboard', username=username))
     return redirect(url_for('home'))
 
 @app.route('/player_dashboard/<username>', methods=['GET', 'POST'])
 def player_dashboard(username):
-    player_data = next((player for player in players if player['username'] == username), None)
+    player_data = next((player for player in players_list if player['username'] == username), None)
     if not player_data:
         return redirect(url_for('home'))
 
@@ -121,7 +186,6 @@ def player_dashboard(username):
         pass
 
     return render_template('player_dashboard.html', player_data=player_data, characters=characters)
-
 @app.route('/create_character', methods=['GET', 'POST'])
 def create_character():
     if request.method == 'POST':
@@ -182,148 +246,82 @@ def create_character():
 
 @app.route('/dm_dashboard/<username>', methods=['GET', 'POST'])
 def dm_dashboard(username):
-    player_data = next((player for player in players if player['username'] == username), None)
-    if not player_data:
-        return redirect(url_for('home'))
-
-    with driver.session() as session:
-        result = session.run(
-            """
-            MATCH (p:Player)-[:HAS_CHARACTER]->(char:Character)-[:BELONGS_TO_CLASS]->(cl:Class)
-            RETURN p.username AS username, char.name AS name, char.level AS level, cl.name AS class,
-                   char.strength AS strength, char.dexterity AS dexterity,
-                   char.constitution AS constitution, char.intelligence AS intelligence,
-                   char.wisdom AS wisdom, char.charisma AS charisma
-            """
-        )
-        players_characters = {}
-        for record in result:
-            username = record["username"]
-            if username not in players_characters:
-                players_characters[username] = []
-            players_characters[username].append({
-                "name": record["name"], "class": record["class"], "level": record["level"],
-                "strength": record["strength"], "dexterity": record["dexterity"], 
-                "constitution": record["constitution"], "intelligence": record["intelligence"], 
-                "wisdom": record["wisdom"], "charisma": record["charisma"]
-            })
-    
-    return render_template('dm_dashboard.html', player_data=player_data, players_characters=players_characters)
-
-@app.route('/spells', methods=['POST'])
-def spells():
-    school = request.form.get('school')
-    spell_name = request.form.get('name')
-    spells = []
-
-    query = "MATCH (s:Spell)"
-    conditions = []
-    params = {}
-
-    if school:
-        conditions.append("()-[:BELONGS_TO]->(school:School {name: $school})")
-        params['school'] = school
-
-    if spell_name:
-        conditions.append("s.name CONTAINS $spell_name")
-        params['spell_name'] = spell_name
-
-    if conditions:
-        query += " WHERE " + " AND ".join(conditions)
-    
-    query += " RETURN s.name AS name, s.level AS level, school.name AS school"
-
-    with driver.session() as session:
-        result = session.run(query, params)
-        spells = [{"name": record["name"], "level": record["level"], "school": record["school"]} for record in result]
-
-    return render_template('spells.html', spells=spells)
-
-@app.route('/spells/<spell_name>')
-def show_spell(spell_name):
-    spell_details = {}
-    
-    with driver.session() as session:
-        result = session.run(
-            """
-            MATCH (s:Spell {name: $spell_name})
-            OPTIONAL MATCH (s)-[:BELONGS_TO]->(school:School)
-            OPTIONAL MATCH (s)-[:CASTING_TIME]->(ct:CastingTime)
-            OPTIONAL MATCH (s)-[:FROM_SOURCE]->(src:Source)
-            OPTIONAL MATCH (s)-[:HAS_AREA_TAG]->(area:AreaTag)
-            OPTIONAL MATCH (s)-[:HAS_COMPONENT]->(comp:Component)
-            OPTIONAL MATCH (s)-[:HAS_ENTRY]->(entry:Entry)
-            OPTIONAL MATCH (s)-[:HAS_MISC_TAG]->(misc:MiscTag)
-            OPTIONAL MATCH (s)-[:HAS_RANGE]->(range:Range)
-            OPTIONAL MATCH (s)-[:INFLECTS_CONDITION]->(cond:Condition)
-            RETURN s.name AS name, s.level AS level, school.name AS school,
-                   ct.time AS casting_time, src.name AS source,
-                   area.tag AS area, COLLECT(DISTINCT comp.name) AS components,
-                   entry.text AS entry, misc.tag AS misc_tag,
-                   range.distance AS range, cond.name AS condition
-            """,
-            spell_name=spell_name
-        )
-        
-        record = result.single()
-        if record:
-            spell_details = {
-                "name": record["name"],
-                "level": record["level"],
-                "school": record["school"],
-                "casting_time": record["casting_time"],
-                "source": record["source"],
-                "area": record["area"],
-                "components": record["components"],
-                "entry": record["entry"],
-                "misc_tag": record["misc_tag"],
-                "range": record["range"],
-                "condition": record["condition"]
-            }
-    
-    return render_template('show_spell.html', spell=spell_details)
-
-def get_available_classes_and_races():
-    with driver.session() as session:
-        classes_result = session.run("MATCH (c:Class) RETURN c.name AS name")
-        races_result = session.run("MATCH (r:Races) RETURN r.name AS name")
-        
-        classes = [record["name"] for record in classes_result]
-        races = [record["name"] for record in races_result]
-        print('Fetching Classes and Races: ' + str(classes) + ' ' + str(races))
-    return classes, races
+    monsters = get_five_monsters()
+    characters = get_characters_for_testing()
+    conditions=get_conditions()
+    return render_template('dm_dashboard.html', monsters=monsters, characters=characters,conditions=conditions)
 
 @app.route('/character_dashboard/<int:character_id>')
 def character_dashboard(character_id):
-    print('Requesting ' + str(character_id))
     character = get_character_details(character_id)
-    print('Response for: ' + str(character_id) + ' ' + str(character))
     skills = calculate_skills(character)
     return render_template('character_dashboard.html', character=character, skills=skills)
 
 def calculate_skills(character):
-    proficiency_bonus = 2 + (character['level'] - 1) // 4  # assuming proficiency bonus increases at level 5, 9, 13, and 17
-    skills = {
-        'Acrobatics': character['dexterity'] + proficiency_bonus,
-        'Animal Handling': character['wisdom'] + proficiency_bonus,
-        'Arcana': character['intelligence'] + proficiency_bonus,
-        'Athletics': character['strength'] + proficiency_bonus,
-        'Deception': character['charisma'] + proficiency_bonus,
-        'History': character['intelligence'] + proficiency_bonus,
-        'Insight': character['wisdom'] + proficiency_bonus,
-        'Intimidation': character['charisma'] + proficiency_bonus,
-        'Investigation': character['intelligence'] + proficiency_bonus,
-        'Medicine': character['wisdom'] + proficiency_bonus,
-        'Nature': character['intelligence'] + proficiency_bonus,
-        'Perception': character['wisdom'] + proficiency_bonus,
-        'Performance': character['charisma'] + proficiency_bonus,
-        'Persuasion': character['charisma'] + proficiency_bonus,
-        'Religion': character['intelligence'] + proficiency_bonus,
-        'Sleight of Hand': character['dexterity'] + proficiency_bonus,
-        'Stealth': character['dexterity'] + proficiency_bonus,
-        'Survival': character['wisdom'] + proficiency_bonus
+    return {
+        'Acrobatics': character['dexterity'],
+        'Animal Handling': character['wisdom'],
+        'Arcana': character['intelligence'],
+        'Athletics': character['strength'],
+        'Deception': character['charisma'],
+        'History': character['intelligence'],
+        'Insight': character['wisdom'],
+        'Intimidation': character['charisma'],
+        'Investigation': character['intelligence'],
+        'Medicine': character['wisdom'],
+        'Nature': character['intelligence'],
+        'Perception': character['wisdom'],
+        'Performance': character['charisma'],
+        'Persuasion': character['charisma'],
+        'Religion': character['intelligence'],
+        'Sleight of Hand': character['dexterity'],
+        'Stealth': character['dexterity'],
+        'Survival': character['wisdom'],
     }
-    return skills
+
+@socketio.on('next_turn')
+def handle_next_turn(data):
+    socketio.emit('update_turn', data, broadcast=True)
+
+@socketio.on('previous_turn')
+def handle_previous_turn(data):
+    socketio.emit('update_turn', data, broadcast=True)
+
+@app.route('/api/players')
+def get_players():
+    with driver.session() as session:
+        result = session.run("MATCH (p:Player)-[:HAS_CHARACTER]->(c:Character) RETURN p.username AS username, id(c) AS id, c.name AS name, c.hp AS hp, c.ac AS ac, c.spell AS spell")
+        players = [{"username": record["username"], "id": record["id"], "name": record["name"], "hp": record["hp"], "ac": record["ac"], "spell": record["spell"]} for record in result]
+    return jsonify(players)
+
+@app.route('/api/monsters')
+def get_monsters():
+    with driver.session() as session:
+        result = session.run("MATCH (m:Monster) RETURN m.name AS name, m.image AS image, m.hp AS hp, m.ac AS ac")
+        monsters = [{"name": record["name"], "image": record["image"], "hp": record["hp"], "ac": record["ac"]} for record in result]
+    return jsonify(monsters)
+
+def get_all_players():
+    with driver.session() as session:
+        result = session.run("MATCH (p:Player) RETURN p.username AS username")
+        players = [{"username": record["username"]} for record in result]
+    return players
+
+def get_all_monsters():
+    with driver.session() as session:
+        result = session.run("MATCH (m:Monster) RETURN m.name AS name, m.hp_average AS hp, m.dex AS dex, m.con AS con, m.int AS int, m.wis AS wis, m.cha AS cha, m.page AS page")
+        monsters = [{"name": record["name"], "hp": record["hp"], "dex": record["dex"], "con": record["con"], "int": record["int"], "wis": record["wis"], "cha": record["cha"], "page": record["page"]} for record in result]
+    return monsters
+
+@app.route('/players')
+def players():
+    players = get_all_players()
+    return jsonify(players)
+
+@app.route('/monsters')
+def monsters():
+    monsters = get_all_monsters()
+    return jsonify(monsters)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
